c857a308f7282daaba2bf9a41dd52852e86ddfe0
suneelmk <suneelkittali@gmail.com>
2025-05-26 00:09:12 +0530

    yup

--- tec/src/test/java/ds/DandS.java
+++ tec/src/test/java/ds/DandS.java
@@ -4,7 +4,7 @@ public class DandS {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("enamma fakaaa");
+		System.out.println("enamma");
 	}
 
 }
